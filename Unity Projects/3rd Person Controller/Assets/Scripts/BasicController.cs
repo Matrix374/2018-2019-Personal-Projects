ed89eeb4a4160e3bc9e70a55dd920f98ec3ca92a
Matrix374 <rumahirfan@gmail.com>
2018-10-15 23:32:44 +0100

    Project Upload

---
+++ Unity Projects/3rd Person Controller/Assets/Scripts/BasicController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+
+    public float mov_speed = 10.0f;
+    public float jump_force = 10.0f;
+    public float disToGround = 1.0f;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        transform.position += horizontal * Vector3.right * Time.deltaTime * mov_speed;
+        transform.position += vertical * Vector3.forward * Time.deltaTime * mov_speed;
+
+        if (isGrounded())
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity += Vector3.up * jump_force;
+        }
+
+        
+    }
+
+    bool isGrounded()
+    {
+        bool raycast = Physics.Raycast(transform.position, Vector3.down, disToGround);
+
+        if(!raycast)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Unity Projects/3rd Person Controller/Assets/Scripts/TankControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour {
+    //TANK CONTROLS
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+
+    public float rotRate = 360.0f;
+    public float mov_speed = 10.0f;
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        ApplyInput(vertical,horizontal);
+    }
+
+    private void ApplyInput(float moveInput, float turnInput)
+    {
+        Move(moveInput);
+        Turn(turnInput);
+    }
+
+    private void Move(float input)
+    {
+        transform.Translate(Vector3.forward * input * mov_speed * Time.deltaTime);
+    }
+
+    private void Turn(float input)
+    {
+        transform.Rotate(0, rotRate * input * Time.deltaTime, 0);
+    }
+}
